c10ea94c67d15bce80dcc9f903f168d3a5ea5fa9
Damien Carcel <damien.carcel@gmail.com>
2023-04-29 14:49:07 +0200

    Add static analysis tools
    - Eslint
    - Prettier
    - Stylelint
    - Typecheck

--- pages/about.cy.tsx
+++ pages/about.cy.tsx
@@ -3,22 +3,22 @@
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 
-import About from './about'
+import About from './about';
 
 // Cypress Component Test
 describe('<AboutPage />', () => {
   it('should render and display expected content', () => {
     // Mount the React component for the About page
-    cy.mount(<About />)
+    cy.mount(<About />);
 
     // The new page should contain an h1 with "About page"
-    cy.get('h1').contains('About Page')
+    cy.get('h1').contains('About Page');
 
     // Validate that a link with the expected URL is present
     // *Following* the link is better suited to an E2E test
-    cy.get('a[href="/"]').should('be.visible')
-  })
-})
+    cy.get('a[href="/"]').should('be.visible');
+  });
+});
 
 // Prevent TypeScript from reading file as legacy script
-export {}
+export {};
